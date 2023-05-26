9f9a701798fc989f13d88a4cafdefb3f4c6f48aa
BOGDAN <bogdan.pirtica@student.upt.ro>
2023-05-26 19:05:14 +0300

    remove dat gui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import { constants } from '../constants/constants.js';
 import { World } from './World/World.js';
-import * as dat from 'dat.gui';
 
-let gui = new dat.GUI();
+// let gui = new dat.GUI();
 
 async function main() {
     const container = document.querySelector('#scene-container');
@@ -17,13 +16,13 @@ async function main() {
     }
 
     
-    gui.add(constants, 'MAX_BOIDS', 50, 800)
-        .onFinishChange((_) => world.restart())
-        .name("Number");
-    gui.add(constants, 'DISPERSION', 0.1, 0.5).name("Dispersion");
-    gui.add(constants, 'RANGE', 100, 500).name("Visual Range");
-    gui.add(guiControls, "disperse").name("Disperse");
-    gui.add(guiControls, "restart").name("Reset");
+    // gui.add(constants, 'MAX_BOIDS', 50, 800)
+    //     .onFinishChange((_) => world.restart())
+    //     .name("Number");
+    // gui.add(constants, 'DISPERSION', 0.1, 0.5).name("Dispersion");
+    // gui.add(constants, 'RANGE', 100, 500).name("Visual Range");
+    // gui.add(guiControls, "disperse").name("Disperse");
+    // gui.add(guiControls, "restart").name("Reset");
     await world.init();
 
     world.start();
